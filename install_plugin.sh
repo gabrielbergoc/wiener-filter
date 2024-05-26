5f7ccec2b098de0e4da498c94179dfeb1b8bd41f
Gabriel Bergoc <gabriel.bergoc@outlook.com>
2024-05-26 16:30:07 -0300

    configs

---
+++ install_plugin.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# LAB
+# cp -r ~/Documentos/Processamento\ de\ Imagens/code/praticas/$1 ~/.imagej/plugins/
+
+# HOME
+cp -r "$PWD/target/Process_Pixels-0.1.0-SNAPSHOT.jar" /c/ImageJ/plugins/
